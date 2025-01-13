0c233c32e1f9befa8edde3c56ef2c9f013fd84d7
popperur <popperur@gmail.com>
2025-01-13 16:25:24 +0100

    Add rubocop gems and reformat code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +9,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+  User.find_or_create_by!(name: "Joe Market Watcher", email: "joe@mw.com") do |user|
+    user.password = "password"
+    user.password_confirmation = "password"
+  end
+end
